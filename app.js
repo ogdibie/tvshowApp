449e449c1268b6dafdde8d07e763e388b12c70cb
Ruona Dibie <ogheneruona.dibie@gmail.com>
2022-04-22 17:21:50 -0700

    Script to pulls Image and shows them on the container

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", function (e) {
+const imageContainer = document.querySelector(".container");
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = this.elements.searchTerm;
-  console.log(searchTerm.value);
+  imageContainer.innerHTML = "";
+  const { data: shows } = await axios.get(
+    "https://api.tvmaze.com/search/shows",
+    {
+      params: {
+        q: searchTerm.value,
+      },
+    }
+  );
+  addImages(shows);
 });
+
+const addImages = (shows) => {
+  for (let res of shows) {
+    if (res.show.image) {
+      const img = document.createElement("IMG");
+      img.src = res.show.image["medium"];
+      imageContainer.append(img);
+    }
+  }
+};
